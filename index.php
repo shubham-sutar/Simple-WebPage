7f03a945e6793628c774b90b969addd96d88646f
shubham sutar <68117385+shubham-sutar@users.noreply.github.com>
2022-09-17 08:58:52 +0530

    Updated title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>YK Photofilms</title>
+	<title>WebDev</title>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/web.css">
